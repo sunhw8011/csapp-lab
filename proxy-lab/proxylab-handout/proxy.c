ec24c301d0934fd73728f4d39f16b742403887be
sun-hongwei8011 <2686224243@qq.com>
2023-01-29 17:39:02 +0800

    完成parse_uri函数功能

--- proxy-lab/proxylab-handout/proxy.c
+++ proxy-lab/proxylab-handout/proxy.c
@@ -9,8 +9,8 @@
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-void doit(int connfd);
-
+void doit(int fd);
+void parse_uri(char *uri, char *hostname, char *port, char *path);
 
 int main(int argc, char** argv)
 {
@@ -40,6 +40,54 @@ int main(int argc, char** argv)
     return 0;
 }
 
-void doit(int connfd) {
-    
+void doit(int fd) {
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    rio_t client_rio, server_rio;
+
+    char hostname[MAXLINE], port[MAXLINE], path[MAXLINE];
+
+    // 面对客户端，自己是服务器，接收客户端发送的请求
+    Rio_readinitb(&client_rio, fd);
+    if (!Rio_readlineb(&client_rio, buf, MAXLINE)) {
+        return;
+    }
+    sscanf(buf, "%s %s %s", method, uri, version);
+    printf("client uri is: %s\n", uri);   // 调试用
+    parse_uri(uri, hostname, port, path);   // 解析uri中元素
+    printf("hostname: %s\n, port: %s\n, path: %s\n", hostname, port, path);
+
+
+}
+
+void parse_uri(char *uri, char *hostname, char *port, char *path) {
+    // 假设uri: http://localhost:12345/home.html
+    char *hostpos = strstr(uri, "//");
+    if (hostpos==NULL) {
+        // 此种情况下，uri为 /home.html
+        char *pathpos = strstr(uri, "/");
+        if (pathpos != NULL) {
+            strcpy(path, pathpos);
+        }
+        strcpy(port, "80"); //没有包含端口信息，则默认端口为80
+        return;
+    } else {
+        char *portpos = strstr(hostpos+2, ":");
+        // hostpos为：//localhost:12345/home.html
+        // portpos为：:12345/home.html
+        if (portpos != NULL) {
+            int portnum;
+            sscanf(portpos+1, "%d%s", &portnum, path);
+            sprintf(port, "%d", portnum);
+            *portpos = '\0';
+        } else {
+            char *pathpos = strstr(hostpos+2, "/");
+            if (pathpos != NULL) {
+                strcpy(path, pathpos);
+                *pathpos = '\0';
+            }
+            strcpy(port, "80");
+        }
+        strcpy(hostname, hostpos+2);
+    }
+    return;
 }
