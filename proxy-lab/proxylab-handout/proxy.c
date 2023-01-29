8dfa7cb97883dd9bfa2dcc74f470d55ba7ed29e0
sun-hongwei8011 <2686224243@qq.com>
2023-01-29 15:58:53 +0800

    添加main函数中监听内容

--- proxy-lab/proxylab-handout/proxy.c
+++ proxy-lab/proxylab-handout/proxy.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "csapp.h"
+
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
@@ -7,8 +9,37 @@
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-int main()
+void doit(int connfd);
+
+
+int main(int argc, char** argv)
 {
+    int listenfd, connfd;
+    socklen_t clientlen;
+    char hostname[MAXLINE], port[MAXLINE];
+    struct sockaddr_storage clientaddr;
+    
+    if (argc != 2) {
+        fprintf(stderr, "usage :%s <port> \n", argv[0]);
+        exit(1);
+    }
+    signal(SIGPIPE, SIG_IGN);   // 防止收到SIGPIPE信号而过早终止
+
+    listenfd = Open_listenfd(argv[1]);
+    while(1) {
+        clientlen = sizeof(clientaddr);
+	    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
+        Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, 
+                    port, MAXLINE, 0);
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
+        doit(connfd);
+        Close(connfd);
+    }
+    
     printf("%s", user_agent_hdr);
     return 0;
+}
+
+void doit(int connfd) {
+    
 }
